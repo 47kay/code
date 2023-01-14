a7075c24912942fdd346ced0af8d9a7bf3b390c6
47kay <kachiwilson@aol.com>
2023-01-14 15:19:13 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// console.log ("hello world");
+// alert ("WELCOME")
+// confirm ("hello are you sure you want to close the window?");
+// window.prompt ("what is your name?")
+
+var x = 40; //
+
+var y;
+y = 2;
+console.log (x,y);
+
+let z = x + y;
+console.log (z);
+
+const w = x%y;
+console.log (w)
